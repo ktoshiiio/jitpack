2af555002a46034d03897b1e9e9202ee4c313422
ktoshiiio <kaneishi-t@gmo-connect.jp>
2024-03-31 02:53:45 +0900

    windowId added.

--- SmaAdWebView/src/main/java/tech/gmo/tmpflutter_webview_android/call_method/WebViewRunner.kt
+++ SmaAdWebView/src/main/java/tech/gmo/tmpflutter_webview_android/call_method/WebViewRunner.kt
@@ -24,6 +24,7 @@ object WebViewRunner {
 //        )
         val params = hashMapOf<String, Any>(
             "initialUrlRequest" to mapOf("url" to "https://offerwall.stg.smaad.net/wall/560363030?u=test1234"),
+            "windowId" to mapOf("windowId" to "windowId"),
             // その他のパラメータを適宜設定
         )
 
